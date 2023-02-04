5019021e76ae04f296bc75eafbc528645a05a1b3
akhyar <akhyar.pvt@gmail.com>
2023-02-05 00:14:06 +0800

    add footer styles

--- components/organisms/footer/Footer.tsx
+++ components/organisms/footer/Footer.tsx
@@ -1,5 +1,9 @@
-export default function Footer () {
-  return <footer>
-    Footer
-  </footer>
+export default function Footer() {
+  return (
+    <footer className="flex items-center justify-center h-12 px-4 text-neutral-500">
+      <span className="block text-sm text-neutral-500">
+        &copy; {new Date().getFullYear()} K.A apparels
+      </span>
+    </footer>
+  );
 }
