4582e2b79d7b8bc45ee19bd65c3fbfc975ff4c86
akhyar <akhyar.pvt@gmail.com>
2023-02-05 00:39:20 +0800

    push footer to bottom of the page

--- components/organisms/navbar/Navbar.tsx
+++ components/organisms/navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import UserActions from "@/components/molecules/navbar/UserActions";
 
 export default function Navbar() {
   return (
-    <nav className="h-20 sticky top-0 max-w-[1920px] mx-auto flex justify-between items-center text-2xl px-6">
+    <nav className="h-20 sticky top-0 max-w-[1920px] mx-auto w-full flex justify-between items-center text-2xl px-6">
       <CategoryList />
       <Title/>
       <UserActions />
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
-import Footer from '@/components/organisms/footer/Footer'
-import Navbar from '@/components/organisms/navbar/Navbar'
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Footer from "@/components/organisms/footer/Footer";
+import Navbar from "@/components/organisms/navbar/Navbar";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <>
-    <Navbar/>
-    <Component {...pageProps} />
-    <Footer/>
-  </>
+  return (
+    <>
+      <Navbar />
+      <main className="flex-grow">
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </>
+  );
 }
